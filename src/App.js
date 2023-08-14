7f10f6596d4b625af987d67c93f33ba453db9c0e
Navin Panthi <panthinabin341@gmail.com>
2023-08-14 16:01:08 +0545

    sending asynchronous HTTP requests with Redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 import { useSelector } from "react-redux";
 
-
 function App() {
+  const cart = useSelector((state) => state.cart);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  const carts = useSelector((state) => state.cart.itemsList);
-
-  console.log(carts);
+  useEffect(() => {
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://redux-shopping-app-1f3e7-default-rtdb.firebaseio.com/cartItems.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+      const data = await response.json();
+    };
+    sendRequest();
+  }, [cart]);
   return (
     <div className="App">
       {!isLoggedIn && <Auth />}
