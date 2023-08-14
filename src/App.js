ac858b71d49f2277ea96f24993fffda6edcf906c
Navin Panthi <panthinabin341@gmail.com>
2023-08-14 11:51:50 +0545

    cart slice state

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 import { useSelector } from "react-redux";
 
+
 function App() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  console.log(isLoggedIn);
+  const carts = useSelector((state) => state.cart.itemsList);
+
+  console.log(carts);
   return (
     <div className="App">
       {!isLoggedIn && <Auth />}
       {isLoggedIn && <Layout />}
-      {/* <Layout /> */}
     </div>
   );
 }
--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,14 +1,24 @@
 import React from "react";
 import CartItem from "./CartItem";
 import "./Cart.css";
+import { useSelector } from "react-redux";
 const CartItems = () => {
+  const cartItems = useSelector((state) => state.cart.itemsList);
   return (
     <div className="cart-container">
       <h2>Your Cart</h2>
       <ul>
-        <li>
-          <CartItem id={1} price={2500} name={"Macbook"} />
-        </li>
+        {cartItems.map((cartItem, index) => (
+          <li key={index}>
+            <CartItem
+              id={cartItem.id}
+              price={cartItem.price}
+              name={cartItem.name}
+              quantity={cartItem.quantity}
+              total={cartItem.totalPrice}
+            />
+          </li>
+        ))}
       </ul>
     </div>
   );
