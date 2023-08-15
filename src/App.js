0a00bed11817e919961c86c55df1a7165be08421
Navin Panthi <panthinabin341@gmail.com>
2023-08-15 15:04:01 +0545

    notification slice state

--- src/App.js
+++ src/App.js
@@ -2,23 +2,60 @@ import React, { useEffect } from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import Notification from "./components/Notification";
+import { UIActions } from "./store/UISlice";
 
+let isFirstRender = true;
 function App() {
+  const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const notification = useSelector((state) => state.ui.notification);
   useEffect(() => {
+    if (isFirstRender) {
+      isFirstRender = false;
+      return;
+    }
     const sendRequest = async () => {
+      // send state as sending request.
+      dispatch(
+        UIActions.showNotification({
+          open: true,
+          message: "Sending request.",
+          type: "warning",
+        })
+      );
       const response = await fetch(
         "https://redux-shopping-app-1f3e7-default-rtdb.firebaseio.com/cartItems.json",
         { method: "PUT", body: JSON.stringify(cart) }
       );
       const data = await response.json();
+      // send state as request is successful.
+      dispatch(
+        UIActions.showNotification({
+          open: true,
+          message: "Sent request to database successfully.",
+          type: "success",
+        })
+      );
     };
-    sendRequest();
-  }, [cart]);
+    sendRequest().catch((err) => {
+      //send state as error.
+      dispatch(
+        UIActions.showNotification({
+          open: true,
+          message: "Request failed.",
+          type: "error",
+        })
+      );
+    });
+  }, [cart, dispatch]);
   return (
     <div className="App">
+      {notification.open && (
+        <Notification type={notification.type} message={notification.message} />
+      )}
       {!isLoggedIn && <Auth />}
       {isLoggedIn && <Layout />}
     </div>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
 import cartSlice from "./cartSlice";
+import UISlice from "./UISlice";
 
 const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
     cart: cartSlice.reducer,
+    ui: UISlice.reducer,
   },
 });
 export default store;
