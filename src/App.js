1ac0085318b5561ad53e029923d65f16d35a3210
Navin Panthi <panthinabin341@gmail.com>
2023-08-15 15:52:57 +0545

    creating thunk pattern with redux

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from "./components/Layout";
 import { useSelector, useDispatch } from "react-redux";
 import Notification from "./components/Notification";
 import { UIActions } from "./store/UISlice";
+import { sendCartData } from "./store/cartSlice";
 
 let isFirstRender = true;
 function App() {
@@ -17,39 +18,40 @@ function App() {
       isFirstRender = false;
       return;
     }
-    const sendRequest = async () => {
-      // send state as sending request.
-      dispatch(
-        UIActions.showNotification({
-          open: true,
-          message: "Sending request.",
-          type: "warning",
-        })
-      );
-      const response = await fetch(
-        "https://redux-shopping-app-1f3e7-default-rtdb.firebaseio.com/cartItems.json",
-        { method: "PUT", body: JSON.stringify(cart) }
-      );
-      const data = await response.json();
-      // send state as request is successful.
-      dispatch(
-        UIActions.showNotification({
-          open: true,
-          message: "Sent request to database successfully.",
-          type: "success",
-        })
-      );
-    };
-    sendRequest().catch((err) => {
-      //send state as error.
-      dispatch(
-        UIActions.showNotification({
-          open: true,
-          message: "Request failed.",
-          type: "error",
-        })
-      );
-    });
+    // const sendRequest = async () => {
+    //   // send state as sending request.
+    //   dispatch(
+    //     UIActions.showNotification({
+    //       open: true,
+    //       message: "Sending request.",
+    //       type: "warning",
+    //     })
+    //   );
+    //   const response = await fetch(
+    //     "https://redux-shopping-app-1f3e7-default-rtdb.firebaseio.com/cartItems.json",
+    //     { method: "PUT", body: JSON.stringify(cart) }
+    //   );
+    //   const data = await response.json();
+    //   // send state as request is successful.
+    //   dispatch(
+    //     UIActions.showNotification({
+    //       open: true,
+    //       message: "Sent request to database successfully.",
+    //       type: "success",
+    //     })
+    //   );
+    // };
+    // sendRequest().catch((err) => {
+    //   //send state as error.
+    //   dispatch(
+    //     UIActions.showNotification({
+    //       open: true,
+    //       message: "Request failed.",
+    //       type: "error",
+    //     })
+    //   );
+    // });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
   return (
     <div className="App">
