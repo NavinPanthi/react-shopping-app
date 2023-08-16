a0cf7798baabebcb2511c6092382068087f7cbda
Navin Panthi <panthinabin341@gmail.com>
2023-08-16 23:35:14 +0545

    Get HTTP request from redux

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 import { useSelector, useDispatch } from "react-redux";
 import Notification from "./components/Notification";
-import { UIActions } from "./store/UISlice";
-import { sendCartData } from "./store/cartSlice";
+import { sendCartData, fetchData } from "./store/cartAction";
 
 let isFirstRender = true;
 function App() {
@@ -14,43 +13,16 @@ function App() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const notification = useSelector((state) => state.ui.notification);
   useEffect(() => {
+    if (cart.changed) {
+      dispatch(fetchData());
+    }
+  }, [dispatch]);
+  useEffect(() => {
     if (isFirstRender) {
       isFirstRender = false;
       return;
     }
-    // const sendRequest = async () => {
-    //   // send state as sending request.
-    //   dispatch(
-    //     UIActions.showNotification({
-    //       open: true,
-    //       message: "Sending request.",
-    //       type: "warning",
-    //     })
-    //   );
-    //   const response = await fetch(
-    //     "https://redux-shopping-app-1f3e7-default-rtdb.firebaseio.com/cartItems.json",
-    //     { method: "PUT", body: JSON.stringify(cart) }
-    //   );
-    //   const data = await response.json();
-    //   // send state as request is successful.
-    //   dispatch(
-    //     UIActions.showNotification({
-    //       open: true,
-    //       message: "Sent request to database successfully.",
-    //       type: "success",
-    //     })
-    //   );
-    // };
-    // sendRequest().catch((err) => {
-    //   //send state as error.
-    //   dispatch(
-    //     UIActions.showNotification({
-    //       open: true,
-    //       message: "Request failed.",
-    //       type: "error",
-    //     })
-    //   );
-    // });
+
     dispatch(sendCartData(cart));
   }, [cart, dispatch]);
   return (
@@ -65,3 +37,36 @@ function App() {
 }
 
 export default App;
+// const sendRequest = async () => {
+//   // send state as sending request.
+//   dispatch(
+//     UIActions.showNotification({
+//       open: true,
+//       message: "Sending request.",
+//       type: "warning",
+//     })
+//   );
+//   const response = await fetch(
+//     "https://redux-shopping-app-1f3e7-default-rtdb.firebaseio.com/cartItems.json",
+//     { method: "PUT", body: JSON.stringify(cart) }
+//   );
+//   const data = await response.json();
+//   // send state as request is successful.
+//   dispatch(
+//     UIActions.showNotification({
+//       open: true,
+//       message: "Sent request to database successfully.",
+//       type: "success",
+//     })
+//   );
+// };
+// sendRequest().catch((err) => {
+//   //send state as error.
+//   dispatch(
+//     UIActions.showNotification({
+//       open: true,
+//       message: "Request failed.",
+//       type: "error",
+//     })
+//   );
+// });
